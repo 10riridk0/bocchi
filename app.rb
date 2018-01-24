759d7a5e4009648d1d53263050d79aa12d231ff8
Juri <10ririmi0kd@gmail.com>
2018-01-24 23:20:29 +0900

    app.rbにloadCSV追加

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'csv'
 
 get '/' do
     erb :index
 end
 
 get '/chat' do
+    @data = loadCSV
     erb :chat
 end
 
 get '/end' do 
     erb :end
 end
+
+# csv読み込む関数
+def loadCSV
+    csv = CSV.table("public/data.csv")
+    linesCout = File.read("public/data.csv").count("\n")
+    print csv[rand(linesCout)]
+end
+
